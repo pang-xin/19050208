6e4466bb96d193bb0a61e2d1a36f17bfa2890002
mr.x <13720527533@163.com>
2019-10-26 15:21:55 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
+ 
 //客户信息
 Route::prefix('/client')->group(function () {
     Route::get('/create', 'Index\ClientController@create');
@@ -33,9 +33,12 @@ Route::prefix('/service')->group(function () {
     Route::get('create', 'Index\ServiceController@create');//添加页面
     Route::post('create_do', 'Index\ServiceController@create_do');//添加执行
     Route::get('list', 'Index\ServiceController@list');//列表展示
+    Route::get('delete/{id}', 'Index\ServiceController@delete');//删除
+    Route::get('update/{id}', 'Index\ServiceController@update');//修改页面
+    Route::post('update_do/{id}', 'Index\ServiceController@update_do');//修改页面
 });
-=======
+
 Route::get('/create', 'index\HereController@create');
 Route::post('/createDo', 'index\HereController@createDo');
 Route::get('index/list', 'index\HereController@list');
->>>>>>> cb142b7f91793c696867800d4458fcacde9a2b25
+
